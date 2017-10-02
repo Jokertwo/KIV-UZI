795cf399a7fa5479d3aacdbe5c18d252b718fe99
Jokertwo <p.lastovka@seznam.cz>
2017-10-03 00:51:32 +0200

    Reprezentace zápalky
    První návrh, pozdeji bude přidán nejpíš obrázek a změna na JToogleButton

---
+++ src/semestralniPrace/Match.java
@@ -0,0 +1,22 @@
+package semestralniPrace;
+
+import javax.swing.JButton;
+
+public class Match extends JButton{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 286040224829902214L;
+	
+	String id;
+	
+	
+	public Match(String title) {
+		super(title);
+		this.id = title;
+	}
+	
+	
+
+}
