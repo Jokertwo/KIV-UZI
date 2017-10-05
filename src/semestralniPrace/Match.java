91395a25ff5890b145829a350582820e1154eaf8
Jokertwo <p.lastovka@seznam.cz>
2017-10-05 16:36:06 +0200

    Zápalka

--- src/semestralniPrace/Match.java
+++ src/semestralniPrace/Match.java
@@ -2,8 +2,6 @@ package semestralniPrace;
 
 import java.awt.Dimension;
 import javax.swing.JToggleButton;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 
 public class Match extends JToggleButton {
@@ -16,20 +14,12 @@ public class Match extends JToggleButton {
     private int group;
 
 
-    public Match(String title,int group) {
-        super(title);
+    public Match(String title,int group,Pile pile) {
+        super(new MatchAction(title,group));
         this.group = group;
         setToolTipText(Integer.toString(this.group));
         setPreferredSize(new Dimension(70, 30));
-        addActionListener(new MatchAction(this));
-        addChangeListener(new ChangeListener() {
-            
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-        });
+
     }
     public int getGroup(){
         return this.group;
