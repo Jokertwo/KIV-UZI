84b056bfa3747dd8570ef19d50cdb686723086b8
Jokertwo <p.lastovka@seznam.cz>
2017-10-05 16:38:06 +0200

    Revert "Revert "Main""
    This reverts commit 265c5f5d0af76fe8371e5985d7a117e5729a1ed5.

--- src/semestralniPrace/Gui.java
+++ src/semestralniPrace/Gui.java
@@ -1,72 +1,43 @@
 package semestralniPrace;
 
-import java.awt.GridLayout;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+
+public class Gui extends JFrame{
+
+    
+    
+   /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
 
-public class Gui {
-
-    static JFrame frame = new JFrame();
-    static List<Pile> listOfPile = new ArrayList<>();
-    static int countOfPile = 5;
-
-
-    public static void createGame(int countOfPile) {
-
-        for (int i = 0; i < countOfPile; i++) {
-            JPanel panel = new JPanel();
-            Pile pile = new Pile((int) (Math.random() * 6) + 1,i);
-            panel.setLayout(new GridLayout(0, 6, 3, 0));
-            List<Match> list = pile.getMatches();
-            listOfPile.add(pile);
-            for (Match item : list) {
-                panel.add(item);
-            }
-            frame.add(panel);
-        }
-    }
-
-
-    public static void main(String[] args) {
-
-        try {
-            // Set System L&F
-            UIManager.setLookAndFeel(
-                UIManager.getSystemLookAndFeelClassName());
-        } catch (UnsupportedLookAndFeelException e) {
-            // handle exception
-        } catch (ClassNotFoundException e) {
-            // handle exception
-        } catch (InstantiationException e) {
-            // handle exception
-        } catch (IllegalAccessException e) {
-            // handle exception
-        }
-
-        frame.addWindowListener(new java.awt.event.WindowAdapter() {
-            @Override
-            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-                if (JOptionPane.showConfirmDialog(frame, 
-                    "Are you sure to close this window?", "Really Closing?", 
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
-                    System.exit(0);
-                }
-            }
-        });
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.setLayout(new GridLayout(countOfPile, 0, 10, 10));
-        createGame(countOfPile);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-
-    }
-
+public Gui(){
+     setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+     setLayout(new BorderLayout());
+     add(new GameDesk(),BorderLayout.CENTER);
+     pack();
+     setVisible(true);
+     setLocationRelativeTo(null);
+     addWindowListener();
+     
+ }
+    
+    
+  public void addWindowListener() {
+  addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+          if (JOptionPane.showConfirmDialog(null,
+              "Are you sure to close this window?", "Really Closing?",
+              JOptionPane.YES_NO_OPTION,
+              JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+              System.exit(0);
+          }
+      }
+  });
+}   
 }
