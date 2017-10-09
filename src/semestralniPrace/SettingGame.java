2a27033e4a40a10bea33c2d70c68cd5849bd60cd
Jokertwo <p.lastovka@seznam.cz>
2017-10-09 09:52:16 +0200

    Setting
    Nastavení hry

--- src/semestralniPrace/SettingGame.java
+++ src/semestralniPrace/SettingGame.java
@@ -1,20 +1,58 @@
 package semestralniPrace;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import net.miginfocom.swing.MigLayout;
 
-public class SettingGame extends JPanel{
+
+public class SettingGame extends JPanel {
 
     /**
      * 
      */
     private static final long serialVersionUID = 8533696883753184050L;
 
+    private JRadioButton playerVsPlayer;
+    private JRadioButton playerVsComp;
     
+    private JComboBox<String> difficulty;
+    
+    private JPanel panel;
+    private ButtonGroup group;
+    
+    private String[] listOfDifficulty = {"Easy", "Medium","Hard"};
+
+
     public SettingGame() {
-     setLayout(new MigLayout());
-        // TODO Auto-generated constructor stub
+        setLayout(new MigLayout("w 300,h 300"));
+        init();
+        
+        add(playerVsComp);
+        add(difficultyLevel(), "wrap");
+        add(playerVsPlayer,"wrap");
+        
+        
+    }
+
+
+    private void init() {
+        group = new ButtonGroup();
+        playerVsComp = new JRadioButton("Player vs Computer",true);
+        playerVsPlayer = new JRadioButton("Player vs Player");
+
+        group.add(playerVsComp);
+        group.add(playerVsPlayer);
+        
     }
     
-    
+    private JPanel difficultyLevel(){
+        panel = new JPanel();
+        difficulty = new JComboBox<String>(listOfDifficulty);
+        panel.add(difficulty);
+                
+        return panel;
+    }
+
 }
