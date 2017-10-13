b1bc737b11bef2b9c6037a43e5f2046e651c9d6a
Jokertwo <p.lastovka@seznam.cz>
2017-10-13 12:31:53 +0200

    Težký protihráč

---
+++ src/oponent/HardOponent.java
@@ -0,0 +1,23 @@
+package oponent;
+
+import java.util.List;
+
+public class HardOponent implements Oponent{
+
+//    private List<Pair<Integer,Integer>> listOfPileSize;
+    
+    @Override
+    public void run() {
+        startPlay();        
+    }
+
+    @Override
+    public void startPlay() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    
+
+}
