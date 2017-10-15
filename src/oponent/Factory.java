cebaa79754a5e8b6ac25bfe85f5aaf2dcea8e1cf
Jokertwo <p.lastovka@seznam.cz>
2017-10-16 00:27:52 +0200

    Tovarna na protivniky

--- src/oponent/Factory.java
+++ src/oponent/Factory.java
@@ -1,5 +1,27 @@
 package oponent;
 
+import org.apache.log4j.Logger;
+import semestralniPrace.Helper;
+
+
 public class Factory {
+
+    private static final Logger LOG = Logger.getLogger(Factory.class);
+    
+    public Oponent getOponent() {
+
+        String kindOfOponent = Helper.gameSetting.get(Helper.OPONENT);
+        if (kindOfOponent.equals(Helper.HARD)) {
+            LOG.info("Vytvarim tezkeho protivnika.");
+            return new HardOponent();
+        } else if (kindOfOponent.equals(Helper.MEDIUM)) {
+            LOG.info("Vytvarim stredne tezkeho protivnika.");
+            return null;
+        } else {
+            LOG.info("Vytvarim lehkeho protivnika.");
+            return new EasyOponent();
+            
+        }
+    }
 
 }
