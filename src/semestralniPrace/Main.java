619e02fa586e09cc2e7368a0ab97b7df9d7a4fd1
Jokertwo <p.lastovka@seznam.cz>
2017-10-16 00:28:39 +0200

    Main
    pridani nastaveni logger

--- src/semestralniPrace/Main.java
+++ src/semestralniPrace/Main.java
@@ -3,12 +3,13 @@ package semestralniPrace;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import org.apache.log4j.BasicConfigurator;
 
 public class Main {
     
     
     public static void main(String[] args) {
-
+        BasicConfigurator.configure();
         try {
             // Set System L&F
             UIManager.setLookAndFeel(
